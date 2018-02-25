8270999f46c077247eaa2350d9c0715a5681284b
Ian Busko <ian.busko@gmail.com>
2018-02-24 20:58:54 -0500

    Update Profile Picture

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Ian Busko`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -27,11 +27,7 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
-            You should follow him on Twitter
-          </a>
+          Written by <strong>Ian Busko</strong> who lives in Lancaster, Pennsylvania.
         </p>
       </div>
     )
